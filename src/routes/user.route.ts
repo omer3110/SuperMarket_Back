87a3a660608cb74e3df34febb043ace4f245893a
omer3110 <omer.sidi10@gmail.com>
2024-08-11 13:36:31 +0300

    users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,40 @@
 import { Router } from "express";
-import { register, login, getUserById } from "../controllers/user.controller";
+import {
+  register,
+  login,
+  getUserById,
+  addProductToCurrentCart,
+  updateProductQuantityInCurrentCart,
+  //   deleteProductFromCurrentCart,
+} from "../controllers/user.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
+// Route to handle user registration
 router.post("/register", register);
+
+// Route to handle user login
 router.post("/login", login);
-router.get("/:id", getUserById);
+
+// Route to get user by ID
+router.get("/:id", verifyToken, getUserById);
+
+// Route to add a product to the current cart
+router.post("/current-cart/add", verifyToken, addProductToCurrentCart);
+
+// Route to update the quantity of a product in the current cart
+router.put(
+  "/current-cart/update",
+  verifyToken,
+  updateProductQuantityInCurrentCart
+);
+
+// Route to delete a product from the current cart
+// router.delete(
+//   "/current-cart/delete",
+//   verifyToken,
+//   deleteProductFromCurrentCart
+// );
 
 export default router;
