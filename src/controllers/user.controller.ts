7236127b500a9ab876ffc47ce28b01f1a0175292
EdenR7 <edenrot777@gmail.com>
2024-08-11 20:47:59 +0300

    Eden-current card

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,14 +4,37 @@ import { Types } from "mongoose";
 import { CartProductI } from "../types/userTypes";
 import { AuthRequest } from "../types/auth.types";
 import { getErrorData } from "../utils/errors/ErrorsFunctions";
+import { BRANDS_NAMES } from "../constants/products.constants";
+
+export function validateProductPrices(productPrices: any): boolean {
+  if (
+    !productPrices ||
+    !Array.isArray(productPrices) ||
+    productPrices.length === 0
+  ) {
+    return false;
+  }
+  for (const product of productPrices) {
+    if (!product.brandName || isNaN(Number(product.price))) {
+      return false;
+    }
+    if (!BRANDS_NAMES.includes(product.brandName)) {
+      return false;
+    }
+  }
+  return true;
+}
 
 export const addProductToCurrentCart = async (
   req: AuthRequest,
   res: Response
 ) => {
-  const { productId, productName, quantity } = req.body;
-  if (!productId || !productName || !quantity) {
+  const { productId, productName, quantity, productPrices } = req.body;
+  if (!productId || !productName || !quantity || !productPrices) {
     return res.status(400).json({ message: "Missing required fields" });
+  }
+  if (!validateProductPrices(productPrices)) {
+    return res.status(400).json({ message: "Invalid product prices" });
   }
 
   try {
@@ -26,13 +49,15 @@ export const addProductToCurrentCart = async (
     );
 
     if (productIndex > -1) {
-      user.currentCart[productIndex].quantity += quantity;
+      return res.status(400).json({ message: "Product Already exist" });
     } else {
-      user.currentCart.push({
+      const newProduct: CartProductI = {
         productId,
         productName,
+        productPrices,
         quantity,
-      } as CartProductI);
+      };
+      user.currentCart.push(newProduct);
     }
 
     await user.save();
@@ -110,10 +135,7 @@ export const clearCurrentCart = async (req: AuthRequest, res: Response) => {
 
     await user.save();
 
-    res.status(200).json({
-      message: "Cart cleared successfully",
-      currentCart: user.currentCart,
-    });
+    res.status(200).json(user.currentCart);
   } catch (error) {
     const { errorMessage, errorName } = getErrorData(error);
     console.log(errorName, errorMessage);
@@ -123,7 +145,7 @@ export const clearCurrentCart = async (req: AuthRequest, res: Response) => {
 
 export const getCartComparison = async (req: AuthRequest, res: Response) => {
   try {
-    // const products = 
+    // const products =
   } catch (error) {
     const { errorMessage, errorName } = getErrorData(error);
     console.log(errorName, errorMessage);
