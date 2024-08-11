04aae95ae9d2ae8f3c40b712eb1670ea8cae75e5
EdenR7 <edenrot777@gmail.com>
2024-08-12 00:22:11 +0300

    Eden

---
+++ src/config/sockets.ts
@@ -0,0 +1,20 @@
+import express, { Application } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app: Application = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`index: connected`, socket.id);
+  socket.on("disconnect", () => {
+    console.log(`index: disconnected`, socket.id);
+  });
+});
+
+export { app, io, server };
---
+++ src/controllers/rooms.controller.ts
@@ -0,0 +1,194 @@
+import { Response } from "express";
+import { AuthRequest } from "../types/auth.types";
+import RoomModel from "../models/rooms.model";
+import { getErrorData } from "../utils/errors/ErrorsFunctions";
+import UserModel from "../models/user.model";
+
+export async function verifyCollaborator(userId: string, roomId: string) {
+  const room = await RoomModel.findOne({ roomId, collaborators: userId });
+  return room ? true : false;
+}
+export async function verifyAdmin(userId: string, roomId: string) {
+  const room = await RoomModel.findOne({ roomId, admin: userId });
+  return room ? true : false;
+}
+
+export async function createRoom(req: AuthRequest, res: Response) {
+  const { roomId, todoCart } = req.body; // should be the socket id
+  const activeCart = todoCart || [];
+  if (!roomId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const admin = req.userId;
+    const collaborators: string[] = [];
+    const room = new RoomModel({
+      roomId,
+      admin,
+      collaborators,
+      todoCart: activeCart,
+    });
+    await room.save();
+    res.status(201).json(room);
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("addCollaborator error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function addCollaborator(req: AuthRequest, res: Response) {
+  const { roomId } = req.params;
+  const { collaboratorId } = req.body;
+  if (!collaboratorId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const room = await RoomModel.findOne({ roomId });
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    if (!verifyAdmin(req.userId as string, roomId)) {
+      return res.status(403).json({ message: "You are not the admin" });
+    }
+
+    if (room.collaborators.includes(collaboratorId)) {
+      return res.status(400).json({ message: "Collaborator already added" });
+    }
+
+    const collaborator = await UserModel.findById(collaboratorId);
+    if (!collaborator)
+      return res.status(404).json({ message: "Collaborator not found" });
+
+    room.collaborators.push(collaboratorId);
+    await room.save();
+
+    res.status(200).json(room);
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("addCollaborator error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getUserRooms(req: AuthRequest, res: Response) {
+  try {
+    const rooms = await RoomModel.findOne({
+      admin: req.userId || { collaborators: req.userId },
+    });
+    res.status(200).json(rooms);
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("addCollaborator error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function toggleProduct(req: AuthRequest, res: Response) {
+  const { roomId, productId } = req.params;
+
+  if (
+    !verifyAdmin(req.userId as string, roomId) &&
+    !verifyCollaborator(req.userId as string, roomId)
+  ) {
+    return res
+      .status(403)
+      .json({ message: "You are not the admin or collaborator" });
+  }
+
+  try {
+    const room = await RoomModel.findOne({ roomId });
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    if (!verifyAdmin(req.userId as string, roomId)) {
+      return res.status(403).json({ message: "You are not the admin" });
+    }
+
+    const productIndex = room.todoCart.findIndex(
+      (product) => product.productId === productId
+    );
+    if (productIndex === -1) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    room.todoCart[productIndex].isActive =
+      !room.todoCart[productIndex].isActive;
+    await room.save();
+
+    res.status(200).json(room);
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("addCollaborator error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function updateQuantity(req: AuthRequest, res: Response) {
+  const { roomId, productId } = req.params;
+  const { quantity } = req.body;
+  if (!quantity || isNaN(Number(quantity)) || quantity < 0) {
+    return res.status(400).json({ message: "Quantity value not valid" });
+  }
+
+  if (
+    !verifyAdmin(req.userId as string, roomId) &&
+    !verifyCollaborator(req.userId as string, roomId)
+  ) {
+    return res
+      .status(403)
+      .json({ message: "You are not the admin or collaborator" });
+  }
+
+  try {
+    const room = await RoomModel.findOne({ roomId });
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    if (!verifyAdmin(req.userId as string, roomId)) {
+      return res.status(403).json({ message: "You are not the admin" });
+    }
+
+    const productIndex = room.todoCart.findIndex(
+      (product) => product.productId === productId
+    );
+    if (productIndex === -1) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    room.todoCart[productIndex].quantity = quantity;
+    await room.save();
+
+    res.status(200).json(room);
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("addCollaborator error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteRoom(req: AuthRequest, res: Response) {
+  const { roomId } = req.params;
+  try {
+    const room = await RoomModel.findOne({ roomId });
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    if (!verifyAdmin(req.userId as string, roomId)) {
+      return res.status(403).json({ message: "You are not the admin" });
+    }
+
+    await RoomModel.deleteOne({ roomId });
+    res.status(200).json({ message: "Room deleted" });
+  } catch (error) {
+    const { errorMessage, errorName } = getErrorData(error);
+    console.log("deleteRoom error: ", errorName, errorMessage);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
